a54d70624466c49ba82e30a45da4cfb0051c8856
soooh <Elli-v>
2021-08-24 17:21:40 +0900

    0824

--- src/main/java/week1_00/sub04.java
+++ src/main/java/week1_00/sub04.java
@@ -1,25 +1,37 @@
 package week1_00;
 
 class information {
-    String name = "제인";
-    int age = 30;
-    String sex = "여성";
-    String married = "기혼자";
-    int child = 1;
+    String name;
+    int age;
+    String sex;
+    String married;
+    int child;
+
+    public information(){
+        this("-", 0, "-", "-", 0);
+    }
+
+    public information(String name){
+        this(name, 30, "여성", "기혼", 1);
+    }
+
+    public information(String name, int age, String sex, String married, int child){
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+        this.married = married;
+        this.child = child;
+    }
 
     public void print() {
         System.out.println("이름은 "+name+"이고 나이는 "+age+"살 입니다.");
-        System.out.println("성별은 "+sex+"이며, "+married+" 입니다. 자녀는 "+child+"명이 있습니다.");
+        System.out.println("성별은 "+sex+"이며, "+married+"자 입니다. 자녀는 "+child+"명이 있습니다.");
     }
 }
 
 public class sub04 {
-    void run(){
-        information st = new information();
-        st.print();
-    }
     public static void main(String[] args) {
-        sub04 sub04 = new sub04();
-        sub04.run();
+        information people = new information("제인");
+        people.print();
     }
 }
--- src/main/java/week1_00/sub04_scan.java
+++ src/main/java/week1_00/sub04_scan.java
@@ -9,21 +9,34 @@ class information_scan {
     String married;
     int child;
 
-    public void read(Scanner scan) {
-        System.out.println("당신의 이름은 무엇인가요?");
-        name = scan.next();
-        System.out.println("나이를 입력해 주세요 (숫자만)");
-        age = scan.nextInt();
-        System.out.println("성별이 무엇인가요 (여성 or 남성)");
-        sex = scan.next();
-        System.out.println("기혼자 or 미혼자");
-        married = scan.next();
-        System.out.println("자녀가 몇 명 있나요? (숫자만)");
-        child = scan.nextInt();
+    public information_scan(){
+        this("-", 0, "-", "-", 0);
     }
+
+    public void read(Scanner scan){
+        System.out.println("당신의 이름은 무엇인가요?");
+        this.name = scan.next();
+        System.out.println("나이를 입력해 주세요 (숫자만)");
+        this.age = scan.nextInt();
+        System.out.println("성별이 무엇인가요 (여성 or 남성)");
+        this.sex = scan.next();
+        System.out.println("기혼 or 미혼");
+        this. married = scan.next();
+        System.out.println("자녀가 몇 명 있나요? (숫자만)");
+        this. child = scan.nextInt();
+    }
+
+    public information_scan(String name, int age, String sex, String married, int child){
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+        this.married = married;
+        this.child = child;
+    }
+
     public void print() {
         System.out.println("이름은 "+name+"이고 나이는 "+age+"살 입니다.");
-        System.out.println("성별은 "+sex+"이며, "+married+" 입니다. 자녀는 "+child+"명이 있습니다.");
+        System.out.println("성별은 "+sex+"이며, "+married+"자 입니다. 자녀는 "+child+"명이 있습니다.");
     }
 }
 
@@ -39,4 +52,3 @@ public class sub04_scan {
         sub04_scan.run();
     }
 }
-//디폴트생성자 공부 더 해볼것!!
